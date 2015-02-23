84b2160211dfe74f4ee8b416a172b36a3882b50c
Zach Dennis <zach.dennis@gmail.com>
2015-02-22 21:35:34 -0500

    Moving 'cd' builtin to builtins/ and added 'popd' alias.
    There is no reason you should have to remember to 'pushd' onto the directory stack like in bash. Your history should simply be known and popd should work to traverse your directory navigation history.

---
+++ lib/yap/shell/builtins/cd.rb
@@ -0,0 +1,21 @@
+module Yap::Shell
+  module Builtins
+    DIRECTORY_HISTORY = []
+
+    builtin :cd do |path=ENV['HOME'], *_|
+      DIRECTORY_HISTORY << Dir.pwd
+      Dir.chdir(path)
+      output=""
+    end
+
+    builtin :popd do
+      output = []
+      if DIRECTORY_HISTORY.any?
+        Dir.chdir(DIRECTORY_HISTORY.pop)
+      else
+        output << "popd: directory stack empty\n"
+      end
+      output.join("\n")
+    end
+  end
+end
