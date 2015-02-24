59b4f6bb42d13fe21d3b4bb594ffcb4c231ee028
Zach Dennis <zach.dennis@gmail.com>
2015-02-23 20:08:55 -0500

    Be sure to close stdout/stderr when running a builtin command.
    This ensures the output doesn't hang:
    
         alias | grep i

--- lib/yap/shell/execution/builtin_command_execution.rb
+++ lib/yap/shell/execution/builtin_command_execution.rb
@@ -3,6 +3,8 @@ module Yap::Shell::Execution
     on_execute do |command:, n:, of:|
       command_output = command.execute
       @stdout.write command_output
+      @stdout.close if @stdout != $stdout && !@stdout.closed?
+      @stderr.close if @stderr != $stderr && !@stderr.closed?
     end
   end
 end
