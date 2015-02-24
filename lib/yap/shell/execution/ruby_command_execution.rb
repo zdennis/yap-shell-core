4fc1c8210de37d5174de9db41118af072ec61bf0
Zach Dennis <zach.dennis@gmail.com>
2015-02-23 19:06:00 -0500

    Update ruby command execution to return an execution status.
    This is so you can chain ruby commands:
     
        !to_s && echo "foo"
        !this-fails && echo "not called"

--- lib/yap/shell/execution/ruby_command_execution.rb
+++ lib/yap/shell/execution/ruby_command_execution.rb
@@ -1,13 +1,14 @@
 module Yap::Shell::Execution
   class RubyCommandExecution < CommandExecution
     on_execute do |command:, n:, of:|
+      result = nil
       stdin, stdout, stderr, world = @stdin, @stdout, @stderr, @world
       t = Thread.new {
         exit_code = 0
         first_command = n == 1
 
         f = nil
-        result = nil
+        ruby_result = nil
         begin
           ruby_command = command.to_executable_str
 
@@ -38,14 +39,14 @@ module Yap::Shell::Execution
 
           if ruby_command =~ /^[A-Z0-9]|::/
             puts "Evaluating #{ruby_command.inspect} globally" if ENV["DEBUG"]
-            result = eval ruby_command
+            ruby_result = eval ruby_command
           else
             ruby_command = "self.#{ruby_command}"
             puts "Evaluating #{ruby_command.inspect} on #{obj.inspect}" if ENV["DEBUG"]
-            result = obj.instance_eval ruby_command
+            ruby_result = obj.instance_eval ruby_command
           end
         rescue Exception => ex
-          result = <<-EOT.gsub(/^\s*\S/, '')
+          ruby_result = <<-EOT.gsub(/^\s*\S/, '')
             |Failed processing ruby: #{ruby_command}
             |#{ex}
             |#{ex.backtrace.join("\n")}
@@ -56,11 +57,11 @@ module Yap::Shell::Execution
         end
 
         # The next line  causes issues sometimes?
-        # puts "WRITING #{result.length} bytes" if ENV["DEBUG"]
-        result = result.to_s
-        result << "\n" unless result.end_with?("\n")
+        # puts "WRITING #{ruby_result.length} bytes" if ENV["DEBUG"]
+        ruby_result = ruby_result.to_s
+        ruby_result << "\n" unless ruby_result.end_with?("\n")
 
-        stdout.write result
+        stdout.write ruby_result
         stdout.flush
         stderr.flush
 
@@ -74,11 +75,11 @@ module Yap::Shell::Execution
         Thread.pass
 
         # Make up an exit code
-        exec_result = Result.new(status_code:exit_code, directory:Dir.pwd, n:n, of:of)
-        exec_result
+        result = Result.new(status_code:exit_code, directory:Dir.pwd, n:n, of:of)
       }
       t.abort_on_exception = true
-      t
+      t.join
+      result
     end
   end
 end
