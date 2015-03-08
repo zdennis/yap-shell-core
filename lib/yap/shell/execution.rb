3380a3cf4dc59e3681400920522e77114dd50182
Zach Dennis <zach.dennis@gmail.com>
2015-03-08 12:49:56 -0400

    Removing auto-loading in favor of simpler requires.

--- lib/yap/shell/execution.rb
+++ lib/yap/shell/execution.rb
@@ -1,15 +1,13 @@
+require "yap/shell/execution/context"
+require "yap/shell/execution/command_execution"
+require "yap/shell/execution/builtin_command_execution"
+require "yap/shell/execution/file_system_command_execution"
+require "yap/shell/execution/ruby_command_execution"
+require "yap/shell/execution/shell_command_execution"
+require "yap/shell/execution/result"
+
 module Yap::Shell
   module Execution
-    autoload :Context,                    "yap/shell/execution/context"
-
-    autoload :CommandExecution,           "yap/shell/execution/command_execution"
-    autoload :BuiltinCommandExecution,    "yap/shell/execution/builtin_command_execution"
-    autoload :FileSystemCommandExecution, "yap/shell/execution/file_system_command_execution"
-    autoload :RubyCommandExecution,       "yap/shell/execution/ruby_command_execution"
-    autoload :ShellCommandExecution,      "yap/shell/execution/shell_command_execution"
-
-    autoload :Result,                     "yap/shell/execution/result"
-
     Context.register BuiltinCommandExecution,    command_type: :BuiltinCommand
     Context.register FileSystemCommandExecution, command_type: :FileSystemCommand
     Context.register ShellCommandExecution,      command_type: :ShellCommand
