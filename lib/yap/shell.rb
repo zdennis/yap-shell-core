532d59b9bc30e97be1ce5bdd13c3737816c4aad5
Zach Dennis <zach.dennis@gmail.com>
2015-03-08 21:42:02 -0400

    Putting history into a world-addon.
    Playing around with what that interface might look like.
    
    Also, wrapping RC file loading into a similar module.

--- lib/yap/shell.rb
+++ lib/yap/shell.rb
@@ -51,22 +51,6 @@ module Yap
         end
       end
 
-      private
-
-      def history_file
-        File.expand_path('~') + '/.yap-history'
-      end
-
-      def load_history
-        return unless File.exists?(history_file) && File.readable?(history_file)
-        (YAML.load_file(history_file) || []).each do |item|
-          ::Readline::HISTORY.push item
-        end
-
-        at_exit do
-          File.write history_file, ::Readline::HISTORY.to_a.to_yaml
-        end
-      end
     end
 
   end
