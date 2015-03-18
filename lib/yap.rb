ee73c53a92e38e06b39d2090519769b3fd9d5c18
Zach Dennis <zach.dennis@gmail.com>
2015-03-17 23:20:45 -0400

    Refactoring how world addons are loaded so they can be added from subdirectories.
    e.g. "~/.yap-addons/some_dir/bar.rb" will try to load Yap::WorldAddons::SomeDir::Bar

--- lib/yap.rb
+++ lib/yap.rb
@@ -8,14 +8,14 @@ module Yap
       $?.exitstatus == 0
     end
 
-    def self.load_from_files(files:[])
+    def self.load_addons_from_files(dir:, files:[])
       files.map do |file|
         (puts "Cannot load world addon: #{file} does not exist" and next) unless File.exist?(file)
         (puts "Cannot load world addon: #{file} is not readable" and next) unless File.exist?(file)
         (puts "Cannot load world addon: #{file} is a directory file" and next) if File.directory?(file)
 
-        addon = file.end_with?("rc") ? load_rcfile(file) : load_addon_file(file)
-        addon.load
+        addon = file.end_with?("rc") ? load_rcfile(file:file) : load_addon_file(dir:dir, file:file)
+        addon.load_addon
       end
     end
 
@@ -24,7 +24,7 @@ module Yap
         @contents = contents
       end
 
-      def load
+      def load_addon
         self
       end
 
@@ -33,31 +33,37 @@ module Yap
       end
     end
 
-    def self.load_rcfile(file)
+    def self.load_rcfile(file:)
       RcFile.new IO.read(file)
     end
 
-    def self.load_addon_file(file)
-      name = File.basename(file).sub(/\.[^\.]+$/, "").split(/[_]/).map(&:capitalize).join
+    def self.load_addon_file(dir:, file:)
+      name = file.sub(dir, "").
+        sub(File.extname(file), "").
+        split(File::Separator).
+        map{ |m| m.split(/[_-]/).map(&:capitalize).join }.
+        join("::")
+
+      # name = File.basename(file).sub(dir, "").sub(File.extname(file), "").split(/[_]/).map(&:capitalize).join
       klass_name = "Yap::WorldAddons::#{name}"
 
       load file
 
-      if Yap::WorldAddons.const_defined?(name)
-        Yap::WorldAddons.const_get(name)
-      else
-        raise("Did not find #{klass_name} in #{file}")
+      klass_name.split("::").reduce(Object) do |namespace,name|
+        if namespace.const_defined?(name)
+          namespace.const_get(name)
+        else
+          raise("Did not find #{klass_name} in #{file}")
+        end
       end
     end
   end
 
   def self.run_shell
-    addon_files = Dir[
-      "#{ENV['HOME']}/.yaprc",
-      "#{ENV['HOME']}/.yap-addons/*.rb"
-    ]
+    addons = []
+    addons.push WorldAddons.load_addons_from_files(dir:"#{ENV['HOME']}", files:Dir["#{ENV['HOME']}/.yaprc"])
+    addons.push WorldAddons.load_addons_from_files(dir:"#{ENV['HOME']}/.yap-addons/", files:Dir["#{ENV['HOME']}/.yap-addons/**/*.rb"])
 
-    addons = WorldAddons.load_from_files(files:addon_files)
-    Shell::Impl.new(addons: addons).repl
+    Shell::Impl.new(addons: addons.flatten).repl
   end
 end
