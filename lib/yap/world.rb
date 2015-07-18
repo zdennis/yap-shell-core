2f7defeb8b1702d506831e975a60d1900427a047
Zach Dennis <zach.dennis@gmail.com>
2015-07-18 18:23:08 -0400

    Change the default word break characters to not include forward-slash. This completely borks tab-completion.
    Although this may not be the right solution. Need to think on this some more. Word break characters seem so limiting. Readline had the same limitation.

--- lib/yap/world.rb
+++ lib/yap/world.rb
@@ -16,6 +16,7 @@ module Yap
 
     def initialize(addons:)
       @editor = RawLine::Editor.new do |editor|
+        editor.word_break_characters = " \t\n\"\\'`@$><=;|&{(/"
         editor.history.search_strategy = Yap::Shell::ReplHistorySearch.new
       end
 
