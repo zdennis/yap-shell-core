1195c6a53a125b5af09caa06cad8d58835966e35
Zach Dennis <zach.dennis@gmail.com>
2015-07-21 12:03:18 -0400

    Resolving issue with tab completion on non-space characters that require being escaped if found in a filename.
    E.g. 
    
       mkdir "A&B"
       ls A
    
    Now if you tab, and you should see:
       A\&B/
    
    And not:
       A&B/

--- addons/tab_completion/tab_completion.rb
+++ addons/tab_completion/tab_completion.rb
@@ -33,27 +33,29 @@ class TabCompletion < Addon
     cycle_matches matches
   end
 
+  # +pre_word_context+ is the "lib/" if the user hits tab after typing "ls lib/"
+  # because the current word will be set to ""
   def pre_word_context
-    word_break_characters = editor.word_break_characters.sub(File::Separator, "").sub('\\', '')
-    word_break_character_rgx = /[#{Regexp.escape(word_break_characters)}]/
+    # Work our way backwards thru the text because we can stop as soon as
+    # see a word break character rather than having to keep track of them.
     i = @before_text.length
-    @str = ""
+    str = ""
     loop do
       i -= 1
       ch = @before_text[i]
-      if ch =~ word_break_character_rgx && (i>0 && @before_text[i-1] != '\\')
+      if ch =~ filtered_work_break_characters_rgx && (i>0 && @before_text[i-1] != '\\')
         break
       else
-        @str << ch
+        str << ch
       end
     end
-    @str.reverse
+    str.reverse
   end
 
   def get_filename_completion_matches
     glob = "#{@pre_word_text}#{@word[:text]}*"
     Dir.glob(glob, File::FNM_CASEFOLD).map do |path|
-      text = path.gsub(' ', '\ ')
+      text = path.gsub(filtered_work_break_characters_rgx, '\\\\\1')
       if File.directory?(path)
         OpenStruct.new(type: :directory, text: text.sub(/^#{Regexp.escape(@pre_word_text)}/, ''))
       else
@@ -153,6 +155,16 @@ class TabCompletion < Addon
     term_info.control "sc" # store cursor position
     blk.call
     term_info.control "rc" # restore cursor position
+  end
+
+  # Remove file separator and the back-slash from word break characters when determining
+  # the pre-word-context
+  def filtered_word_break_characters
+    editor.word_break_characters.sub(File::Separator, "").sub('\\', '')
+  end
+
+  def filtered_work_break_characters_rgx
+    /([#{Regexp.escape(filtered_word_break_characters)}])/
   end
 
 end
