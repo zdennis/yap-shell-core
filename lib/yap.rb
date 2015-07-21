7f67107068b066e769a4dd825bc2533ebac61f9b
Zach Dennis <zach.dennis@gmail.com>
2015-07-21 16:55:12 -0400

    Load the yaprc file after add-ons.

--- lib/yap.rb
+++ lib/yap.rb
@@ -4,8 +4,8 @@ require 'yap/world'
 module Yap
   def self.run_shell
     addons = []
-    addons.push World::Addons.load_rcfiles Dir["#{ENV['HOME']}/.yaprc"]
     addons.push World::Addons.load_directories Dir["#{ENV['HOME']}/.yap-addons/*"]
+    addons.push World::Addons.load_rcfiles Dir["#{ENV['HOME']}/.yaprc"]
 
     Shell::Impl.new(addons: addons.flatten).repl
   end
