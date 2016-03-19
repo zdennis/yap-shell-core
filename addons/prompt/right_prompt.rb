59f4adb57cffcfe26034d75ba1c5fd02fa6fc90e
Zach Dennis <zach.dennis@gmail.com>
2016-03-19 12:54:10 -0400

    Taking out right prompt for now.

--- addons/prompt/right_prompt.rb
+++ addons/prompt/right_prompt.rb
@@ -6,13 +6,12 @@ class RightPrompt < Addon
   def initialize_world(world)
     @world = world
 
-    @world.subscribe(:refresh_right_prompt) do |event|
-      @world.right_prompt_text = Time.now.strftime("%H:%M:%S")
-    end
-
-    @world.events.recur(
-      event: { name: "refresh_right_prompt", source: self },
-      interval_in_ms: 1_000
-    )
+    # @world.subscribe(:refresh_right_prompt) do |event|
+    #   @world.right_prompt_text = Time.now.strftime("%H:%M:%S")
+    # end
+    #
+    # @world.events.recur(
+    #   name: "refresh_right_prompt", source: self, interval_in_ms: 1_000
+    # )
   end
 end
