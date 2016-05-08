652e737aef05d03aefe3805529f2640857fa5123
Zach Dennis <zach.dennis@gmail.com>
2016-05-08 15:01:26 -0400

    Updating world/addons debug log statements to be less verbose

--- lib/yap/world/addons.rb
+++ lib/yap/world/addons.rb
@@ -17,21 +17,20 @@ module Yap
         end
 
         def require(name)
-          Treefell['shell'].puts "#{self}.#{__callee__} enter with: #{name.inspect}"
+          Treefell['shell'].puts "addon is requiring: #{name}"
           directory = File.dirname caller[0].split(':').first
           lib_path = File.join directory, "lib"
           support_file = File.join lib_path, "#{name}.rb"
-          Treefell['shell'].puts "#{self}.#{__callee__} determining if #{name.inspect} should be scoped to addon"
           namespace = self.name.split('::').reduce(Object) do |context,n|
             o = context.const_get(n)
             break o if o.is_a?(Namespace)
             o
           end
           if File.exists?(support_file) && namespace
-            Treefell['shell'].puts "#{self}.#{__callee__}(#{name.inspect}) found in addon, loading #{support_file.inspect} into #{namespace}"
+            Treefell['shell'].puts "#{name} is found in addon, loading #{support_file} in context of #{namespace}"
             namespace.module_eval IO.read(support_file), support_file, lineno=1
           else
-            Treefell['shell'].puts "#{self}.#{__callee__} not found in addon, falling back to super(#{name.inspect})"
+            Treefell['shell'].puts "#{name} not found in addon, falling back to super"
             super(name)
           end
         end
@@ -56,31 +55,31 @@ module Yap
       def self.syntax_ok?(file)
         `ruby -c #{file}`
         ($?.exitstatus == 0).tap do |result|
-          Treefell['shell'].puts "#{self}.#{__callee__} syntax_ok?(#{file.inspect}) #{result}"
+          Treefell['shell'].puts "is syntax ok?(#{file.inspect}) #{result}"
         end
       end
 
       def self.load_rcfiles(files)
-        Treefell['shell'].puts "#{self}.#{__callee__} enter with: #{files.inspect}"
+        Treefell['shell'].puts "loading rcfiles #{files.inspect}"
         files.map do |file|
           if File.exists?(file)
             RcFile.new file
           else
-            Treefell['shell'].puts "#{self}.#{__callee__} skipping #{file.inspect}, does not exist"
+            Treefell['shell'].puts "skipping non-existent rcfile #{file.inspect} "
           end
         end.flatten.compact
       end
 
       def self.load_directories(directories)
-        Treefell['shell'].puts "#{self}.#{__callee__} enter with: #{directories.inspect}"
+        Treefell['shell'].puts "loading addon directories: #{directories.inspect}"
         directories.map do |d|
           if File.directory?(d)
             load_directory(d).map(&:new)
           else
-            Treefell['shell'].puts "#{self}.#{__callee__} skipping #{d.inspect}, not a directory."
+            Treefell['shell'].puts "skipping addon directory #{d.inspect}, does not exist or is not a directory"
           end
         end.flatten.compact.tap do |results|
-          Treefell['shell'].puts "#{self}.#{__callee__} returning with: #{results.inspect}"
+          Treefell['shell'].puts "loaded addons: #{results.inspect}"
         end
       end
 
@@ -92,14 +91,14 @@ module Yap
         end
 
         def initialize_world(world)
-          Treefell['shell'].puts "#{self.class}(#{file.inspect})##{__callee__}"
+          Treefell['shell'].puts "initializing rcfile: #{file}"
           world.instance_eval File.read(@file)
         end
       end
 
       def self.load_directory(directory)
         directory = File.expand_path(directory)
-        Treefell['shell'].puts "#{self}.#{__callee__} enter with: #{directory.inspect}"
+        Treefell['shell'].puts "loading addon from directory: #{directory}"
         namespace = File.basename(directory).
           split(/[_-]/).
           map(&:capitalize).join
@@ -118,32 +117,35 @@ module Yap
         end
 
         Yap::World::UserAddons.const_set namespace, addon_module
-        Treefell['shell'].puts "#{self}.#{__callee__} loading addon into namespace: Yap::World::UserAddons::#{namespace}"
+        Treefell['shell'].puts "creating addon namespace: Yap::World::UserAddons::#{namespace}"
 
         lib_path = File.join directory, "lib"
-        Treefell['shell'].puts "#{self}.#{__callee__} prepending path to $LOAD_PATH: #{lib_path}"
+        Treefell['shell'].puts "prepending addon path to $LOAD_PATH: #{lib_path}"
         $LOAD_PATH.unshift lib_path
 
         gemfiles = Dir["#{directory}/Gemfile"]
-        gemfiles.each do |gemfile|
-          Treefell['shell'].puts "#{self}.#{__callee__} evaluating Gemfile: #{gemfile}"
-          eval File.read(gemfile)
+        Treefell['shell'].puts "looking for Gemfile in #{namespace} addon directory: #{directory}"
+        if gemfiles.any?
+          gemfiles.each do |gemfile|
+            Treefell['shell'].puts "loading #{gemfile} for addon"
+            eval File.read(gemfile)
+          end
+        else
+          Treefell['shell'].puts "No Gemfile found for #{namespace} addon"
         end
 
         Dir["#{directory}/*.rb"].map do |addon_file|
           load_file(addon_file, namespace:namespace, dir:directory, addon_module:addon_module)
-        end.tap do |results|
-          Treefell['shell'].puts "#{self}.#{__callee__} returning with #{results.inspect}"
         end
       ensure
         if lib_path
-          Treefell['shell'].puts "#{self}.#{__callee__} removing path from $LOAD_PATH: #{lib_path}"
+          Treefell['shell'].puts "Removing addon #{lib_path} path from $LOAD_PATH"
           $LOAD_PATH.delete(lib_path)
         end
       end
 
       def self.load_file(file, dir:, namespace:, addon_module:)
-        Treefell['shell'].puts "#{self}.#{__callee__} enter with #{file}, dir: #{dir.inspect}, namespace: #{namespace.inspect}, addon_module: #{addon_module}"
+        Treefell['shell'].puts "loading #{namespace} addon file: #{file}"
         klass_name = file.sub(dir, "").
           sub(/^#{Regexp.escape(File::Separator)}/, "").
           sub(File.extname(file.to_s), "").
@@ -160,7 +162,7 @@ module Yap
             raise("Did not find #{klass_name} in #{file}")
           end
         end.tap do |loaded_addon|
-          Treefell['shell'].puts "#{self}.#{__callee__} returning with addon loaded: #{loaded_addon.inspect}"
+          Treefell['shell'].puts "loaded #{File.dirname(file)} as #{loaded_addon.inspect}"
         end
       end
     end
