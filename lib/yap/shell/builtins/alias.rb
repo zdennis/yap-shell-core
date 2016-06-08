4223ac032ae11856781ec528da75985e4d57a53e
Zach Dennis <zach.dennis@gmail.com>
2016-06-07 21:07:25 -0400

    Adding some basic integration test coverage against alias and unaries.

--- lib/yap/shell/builtins/alias.rb
+++ lib/yap/shell/builtins/alias.rb
@@ -4,6 +4,8 @@ module Yap::Shell
   require 'yap/shell/aliases'
 
   module Builtins
+    Color = ::Term::ANSIColor
+
     builtin :alias do |args:, stdout:, **kwargs|
       output = []
       if args.empty?
@@ -18,7 +20,21 @@ module Yap::Shell
       else
         name_eq_value = args.map(&:shellsplit).join(' ')
         name, command = name_eq_value.scan(/^(.*?)\s*=\s*(.*)$/).flatten
+        output << "Setting alias #{name} #{Color.green('done')}"
         Yap::Shell::Aliases.instance.set_alias name, command
+      end
+      stdout.puts output.join("\n")
+    end
+
+    builtin :unalias do |args:, stdout:, **kwargs|
+      output = []
+      if args.empty?
+        output << "Usage: unalias <aliasname>"
+      else
+        args.each do |alias_name|
+          Yap::Shell::Aliases.instance.unset_alias alias_name
+          output << "Removing alias #{alias_name} #{Color.green('done')}"
+        end
       end
       stdout.puts output.join("\n")
     end
--- spec/features/running_single_commands_spec.rb
+++ spec/features/running_single_commands_spec.rb
@@ -24,31 +24,74 @@ describe 'Running single commands' do
     FileUtils.rm_rf "#{tmp_dir}/*"
   end
 
-  it 'runs basic filesystem commands' do
-    touch 'foo.txt'
-    touch 'bar.txt'
-    type 'ls .'
-    enter
+  describe 'fileystem commands' do
+    it 'runs basic filesystem commands' do
+      touch 'foo.txt'
+      touch 'bar.txt'
+      type 'ls .'
+      enter
 
-    very_soon do
-      expect(output).to match(/bar.txt\nfoo.txt\n/m)
-    end
+      very_soon do
+        expect(output).to match(/bar.txt\nfoo.txt\n/m)
+      end
 
-    type 'echo "hello there"'
-    enter
+      type 'echo "hello there"'
+      enter
 
-    very_soon do
-      expect(output).to match(/hello there\n/m)
-    end
-  end
-
-  it 'pipes filesystem commands' do
-    type "echo 'food' | sed -e 's/o/_/g'"
-    enter
-
-    very_soon do
-      expect(output).to match(/^f__d\n/m)
+      very_soon do
+        expect(output).to match(/hello there\n/m)
+      end
     end
   end
 
+  describe 'piping commands' do
+    it 'pipes filesystem commands' do
+      type "echo 'food' | sed -e 's/o/_/g'"
+      enter
+
+      very_soon do
+        expect(output).to match(/^f__d\n/m)
+      end
+    end
+  end
+
+  describe 'aliases' do
+    before do
+      type "alias foo='echo bar'"
+      enter
+      very_soon do
+        expect(output).to match(/Setting alias foo done/)
+      end
+    end
+
+    it 'sets them' do
+      type 'alias | grep foo'
+      enter
+      very_soon do
+        expect(output).to match(/alias foo='echo bar'/)
+      end
+    end
+
+    it 'executes them' do
+      type 'foo'
+      enter
+      very_soon do
+        expect(output).to match(/bar\n/)
+      end
+    end
+
+    it 'unsets them' do
+      type 'unalias foo'
+      enter
+      very_soon do
+        expect(output).to match(/Removing alias foo done\n/)
+      end
+
+      type 'alias | grep foo'
+      enter
+      very_soon do
+        expect(output).to_not match(/alias foo='echo bar'/)
+      end
+    end
+  end
 end
